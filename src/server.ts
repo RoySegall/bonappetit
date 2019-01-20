9c8e00439922aee07135a92682726ccb11bdb935
Roy Segall <roy@segall.io>
2019-01-20 19:29:11 +0200

    Fix?

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,7 @@ import Settings from "./Base/Settings";
 mongoose.connect(Settings.get().MONGO_URL)
     .then(() => {
         app.listen(Settings.get().PORT, () => {
-            console.log("Express server listening on port " + Settings.get().PORT);
         });
     })
     .catch((error) => {
-        console.error(error);
     });
