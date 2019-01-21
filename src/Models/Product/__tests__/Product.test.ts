0c3cda3502e130d2a8fef85ea1be64563e55ad2b
Roy Segall <roy@segall.io>
2019-01-21 22:46:46 +0200

    Some documentation to the code.

--- src/Models/Product/__tests__/Product.test.ts
+++ src/Models/Product/__tests__/Product.test.ts
@@ -7,7 +7,9 @@ const productService = new ProductService();
 const collection = "product_test";
 
 describe("Product service", () => {
+
     beforeAll(async () => {
+        // Connecting to the db.
         await mongoose.connect(Settings.get().MONGO_URL);
 
         // Change the name of the schema for testing.
@@ -16,7 +18,11 @@ describe("Product service", () => {
 
     test("Testing crud operations", async () => {
         expect.assertions(5);
+
+        // Verify we got nothing.
         expect(await productService.getAll()).toEqual([]);
+
+        // Creating and verify loading.
         const entry = await productService.create({name: "Foo"});
         const entries = await productService.getAll();
 
@@ -26,6 +32,7 @@ describe("Product service", () => {
 
         expect(loadedEntry._id).toEqual(loadedEntry._id);
 
+        // Updating a product.
         productService.update(entry._id, {name: "pizza"}, (err, product) => {
             if (err) {
                 console.error(err);
@@ -35,6 +42,7 @@ describe("Product service", () => {
             expect(product.name).toEqual("pizza");
         });
 
+        // Deleting a product.
         await productService.delete(entry._id);
         loadedEntry = await productService.load(entry._id);
 
@@ -42,11 +50,13 @@ describe("Product service", () => {
     });
 
     afterEach(async () => {
+        // Truncate the items. Don't need it for now since there's only one test but keep it for future use.
         mongoose.connection.db.collection(collection).deleteMany({});
     });
 
     afterAll(async (done) => {
         try {
+            // Removing the collection we created and closing the connection to the DB.
             await mongoose.connection.db.dropCollection(collection);
             mongoose.disconnect(done);
         } catch (e) {
