cee2dbd7db1cec15c14c9daf8a4b9bbe2e906d86
Roy Segall <roy@segall.io>
2019-01-23 17:10:42 +0200

    Maybe now?

--- src/Models/Recipe/__test__/Recipe.test.ts
+++ src/Models/Recipe/__test__/Recipe.test.ts
@@ -2,9 +2,16 @@ import * as mongoose from "mongoose";
 import Settings from "../../../Base/Settings";
 import RecipeService from "../RecipeService";
 import RecipeSchema from "../RecipeSchema";
+import ProductService from "../../Product/ProductService";
+import ProductSchema from "../../Product/ProductSchema";
 
 const recipeService = new RecipeService();
-const collection = "recipe_test";
+const productService = new ProductService();
+
+const recipeCollection = "recipe_test";
+const productCollection = "product_test";
+
+const products: any = {};
 
 describe("Recipe service", () => {
 
@@ -13,7 +20,12 @@ describe("Recipe service", () => {
         await mongoose.connect(Settings.get().MONGO_URL);
 
         // Change the name of the schema for testing.
-        recipeService.setRecipe(mongoose.model("Recipe", RecipeSchema, collection));
+        recipeService.setRecipe(mongoose.model("Recipe", RecipeSchema, recipeCollection));
+        productService.setProduct(mongoose.model("Product", ProductSchema, productCollection));
+
+        products.egg = await productService.create({name: 'Egg'});
+        products.butter = await productService.create({name: 'Butter'});
+        products.salt = await productService.create({name: 'Salt'});
     });
 
     test("Testing crud operations", async () => {
@@ -24,42 +36,73 @@ describe("Recipe service", () => {
 
         // Creating and verify loading.
         const entry = await recipeService.create({
-            name: "Foo"
+            name: "Omelette",
+            description: "Making a simple omelette",
+            ingredients: [
+                {
+                    product_id: products.egg._id,
+                    amount: 2,
+                    quantity: 'pieces',
+                },
+                {
+                    product_id: products.butter._id,
+                    amount: 5,
+                    quantity: 'gram',
+                },
+                {
+                    product_id: products.salt._id,
+                    amount: 1,
+                    quantity: 'tbs',
+                },
+            ],
+            steps: [
+                {text: 'Take a pan and hit it'},
+                {text: 'Crack two eggs into a bowel'},
+                {text: 'Add the salt'},
+                {text: 'scramble them together'},
+                {text: 'Once the pan is hot, poor the scramble eggs to the pan'},
+                {text: 'Wait until the scramble eggs solid on the back side and flip it'},
+                {text: 'Repeat again on the previous step'}
+            ],
         });
-        // const entries = await recipeService.getAll();
-        //
-        // expect(entries[0]._id).toEqual(entry._id);
-        //
-        // let loadedEntry = await recipeService.load(entry._id);
-        //
-        // expect(loadedEntry._id).toEqual(loadedEntry._id);
-        //
-        // // Updating a product.
-        // recipeService.update(entry._id, {name: "pizza"}, (err, product) => {
-        //     if (err) {
-        //         console.error(err);
-        //         return;
-        //     }
-        //
-        //     expect(product.name).toEqual("pizza");
-        // });
-        //
-        // // Deleting a product.
-        // await recipeService.delete(entry._id);
-        // loadedEntry = await recipeService.load(entry._id);
-        //
-        // expect(loadedEntry).toBeNull();
+
+        const entries = await recipeService.getAll();
+
+        expect(entries[0]._id).toEqual(entry._id);
+
+        let loadedEntry = await recipeService.load(entry._id);
+
+        expect(loadedEntry._id).toEqual(loadedEntry._id);
+
+        // Updating a product.
+        recipeService.update(entry._id, {name: "pizza"}, (err, product) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            expect(product.name).toEqual("pizza");
+        });
+
+        // Deleting a product.
+        await recipeService.delete(entry._id);
+        loadedEntry = await recipeService.load(entry._id);
+
+        expect(loadedEntry).toBeNull();
     });
 
     afterEach(async () => {
         // Truncate the items. Don't need it for now since there's only one test but keep it for future use.
-        mongoose.connection.db.collection(collection).deleteMany({});
+        mongoose.connection.db.collection(recipeCollection).deleteMany({});
+        mongoose.connection.db.collection(productCollection).deleteMany({});
     });
 
     afterAll(async (done) => {
         try {
-            // Removing the collection we created and closing the connection to the DB.
-            await mongoose.connection.db.dropCollection(collection);
+            // Removing the recipeCollection we created and closing the connection to the DB.
+            await mongoose.connection.db.dropCollection(recipeCollection);
+            await mongoose.connection.db.dropCollection(productCollection);
+            
             mongoose.disconnect(done);
         } catch (e) {
             console.error(e);
