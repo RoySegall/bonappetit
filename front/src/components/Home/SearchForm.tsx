97b7ecb1d88690d69da23670bf72c20b1f996501
Roy Segall <roy@segall.io>
2019-02-05 00:24:41 +0200

    Some work

--- front/src/components/Home/SearchForm.tsx
+++ front/src/components/Home/SearchForm.tsx
@@ -2,11 +2,34 @@ import * as React from "react";
 
 export default class SearchForm extends React.Component<any, any> {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    handleProductSelection = (event) => {
+        event.preventDefault();
+    };
+
     render() {
         return (
             <form>
                 <h3>Search recipes</h3>
+
                 <hr />
+
+                <div className="products-wrapper">
+
+                    <label htmlFor="products">Select the products you currently have:</label>
+
+                    <div className="products" id="products">
+
+                        {this.props.products.map((product, key) => {
+                            return <button key={key} id={product.id} onClick={this.handleProductSelection}>{product.name}</button>
+                        })}
+                    </div>
+                </div>
             </form>
         )
     }
