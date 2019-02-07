2b1b6b8fdfdaf72f1d4781d03550ddcf40464027
Roy Segall <roy@segall.io>
2019-02-07 18:10:12 +0200

    Fixing lint

--- front/src/components/Home/__tests__/Home.test.tsx
+++ front/src/components/Home/__tests__/Home.test.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Home from '../Home';
-import * as TestUtils from 'react-dom/test-utils';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Home from "../Home";
+import * as TestUtils from "react-dom/test-utils";
 
-it('can render and update a counter', () => {
+it("can render and update a counter", () => {
     // Render App in the document
     const appElement: any = TestUtils.renderIntoDocument(
         <Home/>
@@ -11,8 +11,6 @@ it('can render and update a counter', () => {
 
     const appNode = ReactDOM.findDOMNode(appElement);
 
-    console.log(appNode);
-
     // Verify text content
-    expect(appNode.textContent).toContain('Find out something to cook');
+    expect(appNode.textContent).toContain("Find out something to cook");
 });
