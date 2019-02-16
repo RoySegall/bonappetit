345706056feba90ebfd39b34ca9a036cea94517e
Roy Segall <roy@segall.io>
2019-02-16 14:05:22 +0200

    HUH!

--- travis/both/before.sh
+++ travis/both/before.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
 # Setting up the front env.
-cd travis/front
-bash before.sh
-npm run start-dev &
-
-while ! nc -z localhost 8080; do
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
-
-cd -
-
-echo "Front is running"
-
-cd travis/server
-bash before.sh
-npm run dev &
-
-while ! nc -z localhost 3000; do
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
-
-cd -
-
-echo "Server is running"
-
+pwd
+#cd travis/front
+#bash before.sh
+#npm run start-dev &
+#
+#while ! nc -z localhost 8080; do
+#  sleep 0.1 # wait for 1/10 of the second before check again
+#done
+#
+#cd -
+#
+#echo "Front is running"
+#
+#cd travis/server
+#bash before.sh
+#npm run dev &
+#
+#while ! nc -z localhost 3000; do
+#  sleep 0.1 # wait for 1/10 of the second before check again
+#done
+#
+#cd -
+#
+#echo "Server is running"
+#
