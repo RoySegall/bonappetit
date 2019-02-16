28ac373cf8e86a2071447b0765229f94dd6ada7b
Roy Segall <roy@segall.io>
2019-02-16 14:14:08 +0200

    Hope now

--- travis/both/before.sh
+++ travis/both/before.sh
@@ -2,27 +2,31 @@
 
 # Setting up the front env.
 pwd
-#cd travis/front
-#bash before.sh
-#npm run start-dev &
-#
-#while ! nc -z localhost 8080; do
-#  sleep 0.1 # wait for 1/10 of the second before check again
-#done
-#
-#cd -
-#
-#echo "Front is running"
-#
-#cd travis/server
-#bash before.sh
-#npm run dev &
-#
-#while ! nc -z localhost 3000; do
-#  sleep 0.1 # wait for 1/10 of the second before check again
-#done
-#
-#cd -
-#
-#echo "Server is running"
-#
+cd travis/front
+npm i
+npm run start-dev &
+
+while ! nc -z localhost 8080; do
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+cd -
+
+echo "Front is running"
+
+cd travis/server
+
+npm i -g tslint typescript ts-node
+npm i -D jest typescript
+npm i -D ts-jest @types/jest
+cp .env.travis .env
+
+npm run dev &
+
+while ! nc -z localhost 3000; do
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+
+cd -
+
+echo "Server is running"
