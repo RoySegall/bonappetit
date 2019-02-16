1003bd65b3bde7d2c0d2c8edee06662a803ba4b9
Roy Segall <roy@segall.io>
2019-02-16 13:48:31 +0200

    Now?

--- travis/both/before.sh
+++ travis/both/before.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Setting up the front env.
-cd ../front
+cd front
 bash before.sh
 npm run start-dev &
 
@@ -9,15 +9,19 @@ while ! nc -z localhost 8080; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
+cd -
+
 echo "Front is running"
 
-cd ../server
+cd server
 bash before.sh
 npm run dev &
 
 while ! nc -z localhost 3000; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
+
+cd -
 
 echo "Server is running"
 
